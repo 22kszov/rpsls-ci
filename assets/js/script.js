e1a4374f0dc1f2af3d4401196cef00141c0611c6
22kszov <22kszov@gmail.com>
2024-01-29 10:40:09 +0100

    Add check winner, rules, styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ window.onload = function imagePusher() {
     for (let i = 0; i < 5; i++) {
 
         let weapon = document.createElement("img");
-        weapon.setAttribute('id', weapons[i])
+        weapon.setAttribute('id', weapons[i]);
         weapon.src = weapons[i] + ".png";
         weapon.addEventListener("click", selectWeapon);
         document.getElementById("weapons").append(weapon);
@@ -27,6 +27,102 @@ function selectWeapon() {
     opponent = weapons[Math.floor(Math.random() * 5)];
     document.getElementById("opponent-weapon").src = opponent + ".png";
 
-    
+    if (player == opponent) {
+        playerScore++;
+        opponentScore++;
+    } else {
+        if (player == "rock") {
+            if (opponent == "paper") {
+                opponentScore++;
+            } else {
+                if (opponent == "scissors") {
+                    playerScore++
+                } else {
+                    if (opponent == "lizard") {
+                        playerScore++
+                    } else {
+                        if (opponent == "spock") {
+                            opponentScore++
+                        }
+                    }
+                }
+            }
+        } else {
+            if (player == "paper") {
+                if (opponent == "scissors") {
+                    opponentScore++;
+                } else {
+                    if (opponent == "spock") {
+                        playerScore++
+                    } else {
+                        if (opponent == "rock") {
+                            playerScore++
+                        } else {
+                            if (opponent == "lizard") {
+                                opponentScore++
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (player == "scissors") {
+                    if (opponent == "rock") {
+                    opponentScore++;
+                } else {
+                    if (opponent == "paper") {
+                    playerScore++
+                        } else {
+                            if (opponent == "lizard") {
+                                playerScore++
+                            } else {
+                                if (opponent == "spock") {
+                                    opponentScore++
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (player == "lizard") {
+                        if (opponent == "scissors") {
+                            opponentScore++;
+                        } else {
+                            if (opponent == "spock") {
+                                playerScore++
+                            } else {
+                                if (opponent == "paper") {
+                                    playerScore++
+                                } else {
+                                    if (opponent == "rock") {
+                                        opponentScore++
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if (player == "spock") {
+                            if (opponent == "lizard") {
+                                opponentScore++;
+                            } else {
+                                if (opponent == "rock") {
+                                    playerScore++
+                                } else {
+                                    if (opponent == "scissors") {
+                                        playerScore++
+                                    } else {
+                                        if (opponent == "paper") {
+                                            opponentScore++
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    document.getElementById("player-score").innerHTML = playerScore;
+    document.getElementById("opponent-score").innerHTML = opponentScore;
 
 }
