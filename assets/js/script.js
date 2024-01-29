7ec1306a5f389e80e9147822aa0e85983f99b7cd
22kszov <22kszov@gmail.com>
2024-01-29 09:52:05 +0100

    Add weapon selection and opponent random selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,28 @@ let opponentScore = 0;
 
 const weapons = ["rock", "paper", "scissors", "lizard", "spock"]; 
 
-window.onload = function() {
+window.onload = function imagePusher() {
 
     for (let i = 0; i < 5; i++) {
 
         let weapon = document.createElement("img");
-        weapon.id = weapon[i];
+        weapon.setAttribute('id', weapons[i])
         weapon.src = weapons[i] + ".png";
+        weapon.addEventListener("click", selectWeapon);
         document.getElementById("weapons").append(weapon);
 
     }
+
+}
+
+function selectWeapon() {
+
+    player = this.id;
+    document.getElementById("player-weapon").src = player + ".png";
+
+    opponent = weapons[Math.floor(Math.random() * 5)];
+    document.getElementById("opponent-weapon").src = opponent + ".png";
+
+    
 
 }
